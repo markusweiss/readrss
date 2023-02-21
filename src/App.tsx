62b59cc7557d6ea13a99d431fb4e002de7feced5
markusweiss <git@code71.de>
2023-02-21 23:57:23 +0100

    wip: al lot of console debug

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Button from "./components/Button/Button";
 import FeedBox from "./components/FeedBox/FeedBox";
 
 export default function App() {
+
+    /* theme change with local storage */
     const defaultDark = window.matchMedia(
         "(prefers-color-scheme: dark)"
     ).matches;
@@ -13,24 +15,33 @@ export default function App() {
         defaultDark ? "dark" : "light"
     );
 
-    const [feed, setFeed] = useState(() => {
-        return "";
+    const switchTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+    };
+
+    const checkStat = theme === "light" ? true : false;
+
+
+    /* read feed with proxy */
+    let [feed, setFeed] = useState(() => {
+        return false;
     });
 
     const handleChanged = (event) => {
         setFeed(event.target.value);
-        console.log(event.target.value);
+        console.log("EV-TV:::", event.target.value);
     };
 
     const [isActive, showBox] = useState(() => {
         return false;
     });
 
+
     const showFeedBox = () => {
         showBox((current) => !current);
     };
 
-    const checkStat = theme === "light" ? true : false;
     const [items, setItems] = useState([]);
 
     const feedUri = "https://www.netz98.de/feed/";
@@ -39,39 +50,39 @@ export default function App() {
         getRssFeed(feedUri);
     }, []);
 
+    let [spinner, setSpinner] = useState(true);
+
     const sendUri = () => {
+        setSpinner(true);
+        console.log("sp1", spinner);
+        feed = feed ? "https://digitaltechandbusiness.com/feed/" : feed;
+        console.log("feed:::", feed);
         //getRssFeed("https://digitaltechandbusiness.com/feed/");
         getRssFeed(feed);
     };
 
-    const switchTheme = () => {
-        const newTheme = theme === "light" ? "dark" : "light";
-        setTheme(newTheme);
-    };
-
-    const [spinner, setSpinner] = useState(true);
+    /* use proxy to pass through */
 
     const getRssFeed = async (feedUri) => {
-        const res = await fetch(
-            `https://api.rss2json.com/v1/api.json?rss_url=${feedUri}`
-        );
-
-        const contents = await res.json();
-        setItems(contents.items);
-
-        contents.items.length < 1 ? showBox(true) : showBox(false);
-
-        console.log("items", contents)
-
-        setSpinner(false);
-
+        try {
+            const res = await fetch(`https://api.rss2json.com/v1/api.json?rss_url=${feedUri}`)
+            const contents = await res.json();
+            console.log("items", contents);
+            setItems(contents.items);
+            contents.items.length < 1 ? showBox(true) : showBox(false);
+            contents.items.length > 1 ? setSpinner(false) : setSpinner(false);
+            //setSpinner(false);
+            console.log("sp2", spinner)
+        } catch (e) {
+            console.log(e);
+        }
     };
 
-    console.log(items)
+    //console.log(items)
 
     return (
         <>
-            {spinner ? <div className="loading">LOADING ...</div> : ""}
+            {spinner ? <div className="loading">LOADING or NO FEED FOUND...</div> : false}
             <div className="app" data-theme={theme}>
                 <h1 onClick={showFeedBox}>RSS FEED</h1>
                 {isActive ? (
@@ -81,7 +92,8 @@ export default function App() {
                             handleUri={sendUri}
                             defaultText="search"
                         ></FeedBox>
-                        {items.length < 1 ? (
+
+                        {items.length === undefined ? (
                             <div className="noData">
                                 No working feed found ...
                             </div>
@@ -94,7 +106,7 @@ export default function App() {
                 )}
 
                 <Button defaultCheck={checkStat} handleClick={switchTheme} />
-                {items.map((item, index) => {
+                {!spinner && items.map((item, index) => {
                     return (
                         <div className="article" key={index}>
                             <header>
